8fd871388f9459664b03dd265bca27a062a845e5
dygks910910 <dygks910910@daum.net>
2016-06-14 00:53:21 +0900

    setup

---
+++ ScriptsTermProject/setup.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name ='yohan',version='1.0', py_modules = ['main','DaumOpenAPI','testEmail'],author_email = "dygks910910@daum.net")
